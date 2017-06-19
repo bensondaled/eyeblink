92b8b635b75ac524ad23cb2c0150b1194ae2aa6e
Ben Deverett <bendeverett@gmail.com>
2017-06-19 16:21:30 -0400

    building new system

--- main.py
+++ main.py
@@ -1,3 +1,69 @@
-from hardware import DAQOut
+# Imports
+from __future__ import print_function
+from hardware import DAQOut, Camera
+import os, sys, atexit, time
+import numpy as np
+import settings
 
-d = DAQOut(ports=['port0/line0'])
+# Experiment class handles a single run of the experiment
+class Experiment():
+
+    def __init__(self, name='NONAME'):
+
+        # Saving directories
+        self.save_dir = os.path.join(settings.save_dir, name)
+        if not os.path.exists(self.save_dir):
+            os.makedirs(self.save_dir)
+        data_filename = '{}_data.csv'
+        self.data_file = os.path.join(self.save_dir, data_filename)
+
+        # Initialize hardware
+        self.cam = Camera(**settings.cam_params)
+        self.daq = DAQOut(ports=settings.daq_ports)
+
+        # Register events
+        atexit.register(self.end)
+
+        # Runtime vars
+        self.kill_flag = False
+
+    def run(self):
+        
+        # Run initialization
+        self.df_handle = open(self.data_file) # open data logging file
+        self.trial_idx = 0 # keep track of trial index
+
+        # Deliver trials until program is terminated
+        while not self.kill_flag:
+            self.deliver_trial()
+
+    def deliver_trial(self):
+        pass
+
+    def end(self):
+        try:
+            self.df_handle.close()
+        except:
+            pass
+
+        try:
+            self.daq.end()
+        except:
+            pass
+
+        try:
+            self.cam.end()
+        except:
+            pass
+
+
+if __name__ == '__main__':
+    name = ''
+    while not name.isalnum():
+        name = raw_input('Enter a unique experiment name: ')
+        name = name.lower()
+
+    print('Now running experiment:\t', name)
+
+    exp = Experiment(name=name)
+    exp.run()
---
+++ settings.py
@@ -0,0 +1,14 @@
+# Directories
+save_dir            =   '/path/to/save/directory'
+
+# Hardware
+daq_ports           =   [   'port0/line0', 'port0/line1'   ]
+port_names          =   [   'light',       'puff'          ]
+cam_params          =   {}
+
+# Conditioning
+light_start_time    =   1. # seconds, relative to start of trial
+puff_start_time     =   1.2 # seconds, relative to start of trial
+puff_duration       =   0.050 # seconds
+light_duration      =   0.100 # seconds
+iti                 =   10 # seconds (intertrial interval)   
