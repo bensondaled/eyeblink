c59c31f0c575bdb4ae0355f7e113020268f87368
Ben Deverett <bendeverett@gmail.com>
2017-08-07 13:36:05 -0400

    documentation added

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ Eyeblink Conditioning Interface
 
 This repository stores the code for running eyeblink conditioning experiments in mice. Capabilities include daq interfacing, 2-camera acqusition up to 120Hz, live monitoring and closed-loop trial initiation, and interfacing with two-photon microscopes for synchronization. All data are saved to HDF-5.
 
+Usage notes:
+------------
+
+* Due to restrictions imposed by the CLEye driver, the software must be run in a 32-bit python installation if you want to use 2 cameras simultaneously
+
 Contact deverett [at] princeton [dot] edu for more info.
--- hardware/cameras.py
+++ hardware/cameras.py
@@ -1,3 +1,4 @@
+# imports
 from ctypes import c_int, c_void_p, c_char_p, c_float, c_uint16, c_uint32, c_uint8
 from ctypes import Structure, byref
 import os, time, json, sys, warnings, ctypes, logging, threading, h5py, Queue
@@ -5,7 +6,7 @@ import numpy as np
 import multiprocessing as mp
 from util import now,now2
 
-
+# CLEye driver constants
 CLEYE_CODES = dict(
                     # camera sensor parameters
                     auto_gain                   =   0, #[false, true]
@@ -34,12 +35,11 @@ CLEYE_CODES = dict(
                     greyscale                   =   0,
                     colour                      =   1,
                     #CLEyeCameraResolution
-                    qvga                        = 0,
-                    vga                         = 1,
+                    qvga                        =   0,
+                    vga                         =   1,
                 )
 
-############# Special API Structures ################
-
+# Special API structures
 class GUID(Structure):
     _fields_ = [("Data1", c_uint32),
              ("Data2", c_uint16),
@@ -58,7 +58,35 @@ def mp2np(a):
     return np.frombuffer(a.get_obj(), dtype=np.uint8)
 
 class MovieSaver(mp.Process):
+    """A separate python process used to save frames to a file
+    """
     def __init__(self, name, kill_flag, frame_buffer, flushing, query_idx=0, buffer_size=6000, hdf_resize=30000, min_flush=200, n_cams=1, resolution=None):
+        """
+        Initialize a MovieSaver. Most commonly performed by the PSEye class, and thus should not be handled directly.
+
+        Parameters
+        ----------
+        name : str
+            destination filename for saved movie data
+        kill_flag : multiprocessing Value
+            passed down from controller object, used as a simple flag for terminating process
+        frame_buffer : list of multiprocessing Queues
+            passed down from controller object, used to collect frames between acquisition and saving
+        flushing : multiprocessing Value
+            passed down from controller object, used as a simple flag for controlling flush behaviour
+        query_idx : int
+            index of the camera from which to sample frames when camera is queried
+        buffer_size : int
+            number of frames in memory buffer
+        hdf_resize : int
+            increment by which to increase the hdf dataset when it becomes full
+        min_flush : int
+            minimum number of frames in buffer before flushing
+        n_cams : int
+            number of cameras
+        resolution : tuple of ints, or list thereof
+            (x,y) dimensions of frame for each camera
+        """
         super(MovieSaver, self).__init__()
         self.daemon = True
 
@@ -88,6 +116,8 @@ class MovieSaver(mp.Process):
         
         self.start()
     def run(self):
+        """Main method of the process, to be used with standard Process protocols ( i.e. .start() )
+        """
 
         # Setup hdf5 file and datasets
         self.vw_f = h5py.File(self.name,'w')
@@ -169,86 +199,12 @@ class MovieSaver(mp.Process):
         self.vw_f.close()
         self.saving_complete.value = True
 
-class PSEye():
-    """
-    Handles two distinct objects: the PSEye acqusition object, and the PSEye saving object, which run in separate processes
-    """
-    def __init__(self, idx, resolution_mode, frame_rate, color_mode, query_rate=1, query_idx=0, save_name='noname', cleye_params=None, sync_flag=None):
-
-        # CLEye Params; all should be tuples to allow for multiple cameras
-        self.idx                = idx
-        self.resolution_mode    = resolution_mode
-        self.frame_rate         = frame_rate
-        self.color_mode         = color_mode
-        self.cleye_params       = cleye_params
-        # Interfacing params
-        self.query_rate         = query_rate
-        self.query_idx          = query_idx
-        self.save_name          = save_name
-
-        # Special case for scenario where user uses shortcut for single camera, supplying straight params instead of n-length tuples
-        if isinstance(self.idx, int):
-            self.idx = (self.idx,)
-            self.resolution_mode = (self.resolution_mode,)
-            self.frame_rate = (self.frame_rate,)
-            self.color_mode = (self.color_mode,)
-            self.cleye_params = (self.cleye_params,)
-
-        self.n_cams = len(self.idx)
-        for o in [self.resolution_mode, self.frame_rate, self.color_mode, self.cleye_params]:
-            assert len(o) == self.n_cams, 'All supplied camera parameters must be for the same number of cameras.'
-
-        # Inferred params
-        self.resolution = [_PSEye.DIMENSIONS[rm] for rm in self.resolution_mode]
-
-        # Shared variables for acqusition and saving processes
-        self.frame_buffer = [mp.Queue() for _ in range(self.n_cams)]
-        self.kill_flag = mp.Value('b',False)
-        self.saving = mp.Value('b', False)
-        self.flushing = mp.Value('b', False)
-
-        self.saver = MovieSaver(name=self.save_name, resolution=self.resolution, kill_flag=self.kill_flag, frame_buffer=self.frame_buffer, flushing=self.flushing, n_cams=self.n_cams, query_idx=self.query_idx)
-        self.pseye = _PSEye(idx=self.idx, resolution_mode=self.resolution_mode, frame_rate=self.frame_rate, color_mode=self.color_mode, frame_buffer=self.frame_buffer, kill_flag=self.kill_flag, saving_flag=self.saving, sync_flag=sync_flag, query_idx=self.query_idx, cleye_params=self.cleye_params)
-
-        self.last_query = now()            
-
-    def get(self):
-        if now()-self.last_query < 1./self.query_rate:
-            return None,None
-        self.last_query = now()
-
-        # query from saver:
-        #self.saver.query_flag.value = True
-        #fr = mp2np(self.saver.query_queue)
-        #frts = self.saver.query_queue_ts.value
-        
-        # query from _PSEye
-        self.pseye.query_flag.value = True
-        while self.pseye.query_flag.value == True:
-            pass
-        fr = self.pseye.query_queue[0]
-        frts = self.pseye.query_queue_ts.value
-
-        x,y = self.resolution[self.query_idx]
-        return frts,fr.reshape([y,x])
-
-    def set_flush(self, val):
-        self.flushing.value = val
-    
-    def reset_cams(self):
-        self.pseye.reset_cams()
-
-    def end(self):
-        self.kill_flag.value = True
-        while (not self.pseye.thread_complete.value) or (not self.saver.saving_complete.value):
-            pass
-    def begin_saving(self):
-        self.saving.value = True
-        
 class _PSEye(mp.Process):
     """
     An object that runs as its own process, serving the role of containing and calling the PSEye driver API
     Constantly checks for the presence of new frames and dumps them into multiprocessing queue
+
+    This class should not be used directly, but rather will be handled by the PSEye user-friendly class, documented below.
     """
 
     RES_SMALL = CLEYE_CODES['qvga']
@@ -417,10 +373,145 @@ class _PSEye(mp.Process):
             self.dll.CLEyeCameraStart(c)
 
         time.sleep(0.01)
+
+class PSEye():
+    """Camera class for movie acquisition and saving
+    Handles two distinct objects: the PSEye acqusition object, and the PSEye saving object, which run in separate processes
+    """
+    def __init__(self, idx, resolution_mode, frame_rate, color_mode, query_rate=1, query_idx=0, save_name='noname', cleye_params=None, sync_flag=None):
+        """Initialize a PSEye object
+
+        Parameters
+        ----------
+        idx : int, or tuple thereof
+            index/indices of PSEye camera/s to acquire, starting at 0 
+        resolution_mode : int, or tuple thereof
+            resolution mode/s of camera/s to acquire (0=small, 1=large)
+            small: w=320, h=240 pixels
+            large: w=640, h=480 pixels
+            (can be found in CLEYE_CODES, qvga or vga; alternatively in the _PSEye class as RES_SMALL or RES_LARGE)
+        frame_rate : int, or tuple thereof
+            frames per second of camera/s to acquire (available framerates, for large resolution: [15,30,40,50,60,75], small resolution: [15,30,60,75,100,125])
+        color_mode : int, or tuple thereof
+            colour mode of camera/s to acquire (0=greyscale, 1=colour)
+            (can be found in CLEYE_CODES, greyscale or colour; alternatively in the _PSEye class as GREYSCALE or COLOUR)
+        query_rate : int
+            maximum rate (frames/s) at which camera frames can be live-requested for processing (see querying notes below)
+        save_name : str
+            path to destination file for saving
+        cleye_params : dict, or tuple thereof
+            parameters for the camera/s to acquire; see CLEYE_CODES for options
+        sync_flag : multiprocessing Vaule
+            used to synchronize multiple processes; ignored if None
+
+        For example usage of this class, see the example in the main function of this module (bottom of file).
+
+        Note that the querying function is used by calling .get() on an instance of this class. Importantly, there is no guarantee that the frame is being delivered in real time. It is a mechanism for transferring frames with low priority between the acqusition and main processes, but not optimized for consistent real-time usage (though in practice it often does quite well).
+
+        The data are saved to an HDF-5 file, which can be read by any HDF-5 library (in C++, python, MATLAB, etc.)
+        For each camera, there exists a dataset of frames, and a dataset of timestamps. The timestamps are Nx2, for system time and clock.
+        """
+
+        # CLEye Params; all should be tuples to allow for multiple cameras
+        self.idx                = idx
+        self.resolution_mode    = resolution_mode
+        self.frame_rate         = frame_rate
+        self.color_mode         = color_mode
+        self.cleye_params       = cleye_params
+        # Interfacing params
+        self.query_rate         = query_rate
+        self.query_idx          = query_idx
+        self.save_name          = save_name
+
+        # Special case for scenario where user uses shortcut for single camera, supplying straight params instead of n-length tuples
+        if isinstance(self.idx, int):
+            self.idx = (self.idx,)
+            self.resolution_mode = (self.resolution_mode,)
+            self.frame_rate = (self.frame_rate,)
+            self.color_mode = (self.color_mode,)
+            self.cleye_params = (self.cleye_params,)
+
+        self.n_cams = len(self.idx)
+        for o in [self.resolution_mode, self.frame_rate, self.color_mode, self.cleye_params]:
+            assert len(o) == self.n_cams, 'All supplied camera parameters must be for the same number of cameras.'
+
+        # Inferred params
+        self.resolution = [_PSEye.DIMENSIONS[rm] for rm in self.resolution_mode]
+
+        # Shared variables for acqusition and saving processes
+        self.frame_buffer = [mp.Queue() for _ in range(self.n_cams)]
+        self.kill_flag = mp.Value('b',False)
+        self.saving = mp.Value('b', False)
+        self.flushing = mp.Value('b', False)
+
+        self.saver = MovieSaver(name=self.save_name, resolution=self.resolution, kill_flag=self.kill_flag, frame_buffer=self.frame_buffer, flushing=self.flushing, n_cams=self.n_cams, query_idx=self.query_idx)
+        self.pseye = _PSEye(idx=self.idx, resolution_mode=self.resolution_mode, frame_rate=self.frame_rate, color_mode=self.color_mode, frame_buffer=self.frame_buffer, kill_flag=self.kill_flag, saving_flag=self.saving, sync_flag=sync_flag, query_idx=self.query_idx, cleye_params=self.cleye_params)
+
+        self.last_query = now()            
+
+    def get(self):
+        """Query a frame from the camera
+
+        Not guaranteed to work in real time, but if useful for online monitoring of feed
+        Returns at a maximum rate specified in initialization of the object
+        """
+        if now()-self.last_query < 1./self.query_rate:
+            return None,None
+        self.last_query = now()
+
+        # query from saver (an old strategy that may be desired at points): 
+        #self.saver.query_flag.value = True
+        #fr = mp2np(self.saver.query_queue)
+        #frts = self.saver.query_queue_ts.value
+        
+        # query from _PSEye (a newer strategy that is preferable for most uses):
+        self.pseye.query_flag.value = True
+        while self.pseye.query_flag.value == True:
+            pass
+        fr = self.pseye.query_queue[0]
+        frts = self.pseye.query_queue_ts.value
+
+        x,y = self.resolution[self.query_idx]
+        return frts,fr.reshape([y,x])
+
+    def set_flush(self, val):
+        """Set flushing to file on or off
+        
+        Parameters
+        ----------
+        val : bool
+            set flushing on (True) or off (False)
+        """
+        self.flushing.value = val
+    
+    def reset_cams(self):
+        """Convenience method for resetting the camera feeds while class in instantiated and running
+
+        Can be implemented as a fallback method for continuous experiments that cannot be interrupted, in case camera feeds become interrupted
+        """
+        self.pseye.reset_cams()
+
+    def end(self):
+        """End acquisition and saving.
+
+        May take a few seconds.
+        """
+        self.kill_flag.value = True
+        while (not self.pseye.thread_complete.value) or (not self.saver.saving_complete.value):
+            pass
+
+    def begin_saving(self):
+        """Call this method to begin saving to file.
+
+        Camera does not save to file by default, since often there are other tasks to run before saving becomes relevant.
+        """
+        self.saving.value = True
+        
     
 
-##################################################################################################
+###### EXAMPLE USAGE CODE ######
 
+# example (and default) camera settings for 2 cameras
 default_cam_params = dict(  idx=(0,1), 
                             resolution_mode=(_PSEye.RES_SMALL,_PSEye.RES_SMALL), 
                             query_rate = 15,
@@ -432,17 +523,32 @@ default_cam_params = dict(  idx=(0,1),
                                                     auto_whitebalance = True,
                                                     vflip = True,
                                                     hflip = True,
-                                                    rotation = False#-500,
                                                     ),
                                             dict(
                                                     auto_gain = True,
                                                     auto_exposure = True,
                                                     auto_whitebalance = True,
-                                                    
                                                     vflip = True,
                                                     hflip = True,
-                                                    rotation = False#-500,
                                                     )
                                             )
         )
         
+if __name__ == '__main__':
+    """
+    Main function is included here to demonstrate example usage of the camera class outside the software
+    Running this main will generate an HDF-5 file in the specified location
+    """
+
+    # uncomment the following line if you want to use this module in another file
+    # from cameras import PSEye, default_cam_params
+
+    cam_params = default_cam_params.copy() # you can modify this dictionary as desired to set parameters, with options listed in CLEYE_CODES at top of this file
+
+    cam = PSEye(save_name="/path/to/somewhere", cleye_params=cam_params) # init a camera object
+    cam.begin_saving() # begin saving frames to file
+    cam.set_flush(True) # if False, stacks frames in memory until set to True, or until buffer becomes full
+
+    # timestamp,image = cam.get() # can be used to capture a frame within a separate process; see class notes for details
+
+    # cam.end()  # when you want to stop the camera feed
